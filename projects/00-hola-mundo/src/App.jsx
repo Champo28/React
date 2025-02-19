8d78513e730883019ecffc448b0da19f8021fbc6
Champo28 <ayalafranjr4@gmail.com>
2025-02-19 00:15:06 +0000

    Add first class

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { TwitterFollowCard } from './TwitterFollowCard';
 
-function App() {
-  const [count, setCount] = useState(0)
+export function App(){
+    const format = (username) => `@${username}`;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const users = [
+        {
+            userName: 'midudev',
+            name: 'Miguel Angel Durán',
+            isFollowing: true
+        },
+        {
+            userName: 'pheralb',
+            name: 'Pablo Hernandez',
+            isFollowing: false
+        },
+        {
+            userName: 'elonmusk',
+            name: 'Elon Musk',
+            isFollowing: true
+        },
+        {
+            userName: 'vxnder',
+            name: 'Vanderhart',
+            isFollowing: true
+        }
+    ]
+
+    return(
+        <section className="App">
+            {
+                users.map(({ userName, name, isFollowing }) => {
+                    return(
+                        <TwitterFollowCard 
+                            key={userName}
+                            format={format}
+                            userName={userName}
+                            name={name}
+                            initialIsFollowing={isFollowing}
+                        />
+                    );
+                })
+            }
+        </section>
+    );
 }
-
-export default App
