0d5ff9a327dc0f4ed2b06e41da0aced11a6c60de
Champo28 <ayalafranjr4@gmail.com>
2025-02-21 00:03:38 +0000

    LocalStorage implementation

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,14 +1,22 @@
 import { useState } from "react";
 import confetti from "canvas-confetti";
 import { Square } from './components/Square.jsx'
+import { Board } from "./components/Board.jsx";
 import { TURNS } from "./constants.js";
 import { checkWinner, checkEndGame } from "./logic/board.js";
 import { WinnerModal } from "./components/WinnerModal.jsx";
+import { saveGameToStorage, resetGameToStorage } from "./logic/storage/index.js";
 
 function App() {
-	const [board, setBoard] = useState(Array(9).fill(null));
+	const [board, setBoard] = useState(() => {
+		const boardFromStorage = window.localStorage.getItem('board');
+		return boardFromStorage ? JSON.parse(boardFromStorage) : Array(9).fill(null);
+	});
 
-	const [turn, setTurn] = useState(TURNS.X);
+	const [turn, setTurn] = useState(() => {
+		const turnFromStorage = window.localStorage.getItem('turn');
+		return turnFromStorage ?? TURNS.X;
+	});
 
 	const [winner, setWinner] = useState(null);
 
@@ -16,6 +24,8 @@ function App() {
 		setBoard(Array(9).fill(null));
 		setTurn(TURNS.X);
 		setWinner(null);
+
+		resetGameToStorage();
 	}
 
 	const updateBoard = (index) => {
@@ -27,6 +37,9 @@ function App() {
 
 		const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X;
 		setTurn(newTurn);
+
+		//Guardar partida
+		saveGameToStorage({board: newBoard, turn: newTurn});
 
 		const newWinner = checkWinner(newBoard);
 		if(newWinner){
@@ -42,19 +55,8 @@ function App() {
 		<main className="board">
       		<h1>Tic Tac Toe!</h1>
 			<button onClick={resetGame}>Reset del juego</button>
-      		<section className="game">
-        		{board.map((_, index) => {
-          			return(
-            			<Square 
-							key={index}
-							index={index}
-							updateBoard={updateBoard}
-						>
-							{board[index]}
-						</Square>
-          			);
-        		})}
-      		</section>
+			
+			<Board board={board} updateBoard={updateBoard}/>
 
 			<section className="turn">
 				<Square isSelected={turn === TURNS.X}>
