c8a85632fc99c030bfdef003d06fa0a26b42c4fa
Champo28 <ayalafranjr4@gmail.com>
2026-02-26 00:43:57 +0000

    king logic added

--- projects/chess/src/App.jsx
+++ projects/chess/src/App.jsx
@@ -249,8 +249,72 @@ function App() {
 
 	const bishopLogic = (bishop, target) => {
 		const validateMove = generateBishopValidMoves(bishop)
-		if (validateMove.some(square => square.x == target.x && square.y == target.y)) {
+		if (validateMove.some(square => square.x === target.x && square.y === target.y)) {
 			bishop.firstMove = true
+			return true
+		}
+		return false
+	}
+
+	const generateQueenValidMoves = (queen) => {
+		let moves = generateBishopValidMoves(queen)
+		moves.push(generateRookValideMoves(queen))
+		console.log(moves)
+		return moves
+	}
+
+	const queenLogic = (queen, target) => {
+		const validMoves = generateQueenValidMoves(queen)
+		if (validMoves.some(square => square.x === target.x && square.y === target.y)) {
+			queen.firstMove = true
+			return true
+		}
+		return false
+	}
+	const generateKingValidMoves = (king) => {
+		let moves = []
+		const row = selected.row, col = selected.col
+
+		if (row - 1 >= 0 && col - 1 >= 0) {
+			if (board[row - 1][col - 1] === null || board[row - 1][col - 1].color !== king.color) moves.push({ x: row - 1, y: col - 1})
+		}
+
+		if (row - 1 >= 0) {
+			if (board[row - 1][col] === null || board[row - 1][col].color !== king.color) moves.push({ x: row - 1, y: col })
+		}
+
+		if (row - 1 >= 0 && col + 1 <= 7) {
+			if (board[row - 1][col + 1] === null || board[row - 1][col + 1].color !== king.color) moves.push({ x: row - 1, y: col + 1})
+		}
+
+		if (col + 1 <= 7) {
+			if (board[row][col + 1] === null || board[row][col + 1].color !== king.color) moves.push({ x: row, y: col + 1})
+		}
+
+		if (row + 1 <= 7 && col + 1 <= 7) {
+			if (board[row + 1][col + 1] === null || board[row + 1][col + 1].color !== king.color) moves.push({ x: row + 1, y: col + 1})
+		}
+
+		if (row + 1 <= 7) {
+			if (board[row + 1][col] === null || board[row + 1][col].color !== king.color) moves.push({ x: row + 1, y: col })
+		}
+
+		if (row + 1 <= 7 && col - 1 >= 0) {
+			if (board[row + 1][col - 1] === null || board[row + 1][col - 1].color !== king.color) moves.push({ x: row + 1, y: col - 1})
+		}
+
+		if (col - 1 >= 0) {
+			if (board[row][col - 1] === null || board[row][col - 1].color !== king.color) moves.push({ x: row, y: col - 1})
+		}
+
+		console.log(moves)
+		return moves
+	}
+
+	const kingLogic = (king, target) => {
+		const validMoves = generateKingValidMoves(king)
+		if (validMoves.some(square => square.x === target.x && square.y === target.y)) {
+			king.firstMove = true
 			return true
 		}
 		return false
